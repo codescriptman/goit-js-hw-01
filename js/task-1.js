b9d040c1d5e0e2c957f3d6b2e98174ddd5bc2f60
Azazello <donazazello1312@gmail.com>
2025-07-03 02:25:37 +0300

    js path

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,3 @@ function makeTransaction(quantity, pricePerDroid) {
     quantity * pricePerDroid
   } credits!`;
 }
-// quantity— перший параметр, число, що містить кількість замовлених дроїдів
-// pricePerDroid — другий параметр, число, що містить вартість одного дроїда
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-console.log(makeTransaction(88, 88));
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,3 @@
 function getElementWidth(content, padding, border) {
   return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
-// • content — перший параметр, ширина контенту
-// • padding — другий параметр, значення горизонтального падінгу для кожної зі сторін
-// • border — третій параметр, значення товщини бордера для кожної зі сторін Значення всіх параметрів будуть рядками формату Npx де N — це довільне число, ціле або дробове.
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
