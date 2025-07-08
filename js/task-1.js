314be8590ade78392606e2732a6528cc83ed5ee6
Azazello <donazazello1312@gmail.com>
2025-07-08 12:14:04 +0300

    console

--- js/task-1.js
+++ js/task-1.js
@@ -3,3 +3,8 @@ function makeTransaction(quantity, pricePerDroid) {
     quantity * pricePerDroid
   } credits!`;
 }
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(0, 100)); // "You ordered 0 droids worth 0 credits!"
+console.log(makeTransaction(7, 2000)); // "You ordered 7 droids worth 14000 credits!"
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,8 @@
 function getElementWidth(content, padding, border) {
   return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("100px", "20px", "10px")); // 160
+console.log(getElementWidth("150px", "5px", "2px")); // 164
